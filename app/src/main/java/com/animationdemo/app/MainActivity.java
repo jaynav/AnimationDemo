cb8eb8fb16115306338f9158d5ea080cefe7dad4
javier <javier@jnav.us>
2014-08-10 23:36:22 -0700

    Basic structure

---
+++ app/src/main/java/com/animationdemo/app/MainActivity.java
@@ -0,0 +1,64 @@
+package com.animationdemo.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+//on resume goes here
+   @Override
+   public void onResume()
+   {
+        super.onResume();
+   }
+
+//on pause goes here
+  @Override
+    public void onPause()
+    {
+       super.onPause();
+    }
+//on save goes here
+  @Override
+  public void onSaveInstanceState(Bundle outSaveState)
+  {
+      super.onSaveInstanceState(outSaveState);
+  }
+
+//on destroy goes here
+  public void  onDestroy()
+  {
+      super.onDestroy();
+  }
+
+}
