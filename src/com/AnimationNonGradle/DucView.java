1749877f45ffa874a7e02d105545f95e3a5d3d3e
javier <javier@jnav.us>
2014-10-12 03:46:59 -0700

    new hard drive
    took forever to get a new hardrive :(

--- src/com/AnimationNonGradle/DucView.java
+++ src/com/AnimationNonGradle/DucView.java
@@ -19,10 +19,51 @@ public class DucView extends View {
 
     }
 
-    public void start() {
+
+    public Bitmap scale(Bitmap derBase, float derWidth)
+    {return null;// change from null
+
     }
 
-    public void stop() {
+    public void onDraw(Canvas derScreen)
+    {}
+    public boolean onTouchEvent(MotionEvent dahEvent)
+    {return true; // change this too
 
     }
+    private Runnable updateDerState= new Runnable() {
+        @Override
+        public void run() {
+
+        }
+    };
+    public void wrapper(){}
+    public void randomDirection(){}
+    public void start()
+    {
+    }
+
+    public void stop()
+    {
+
+    }
+    //for image
+    private Bitmap derDuc;
+    private Bitmap derAlt;
+    //where it starts
+    private float derX;
+    private float derY;
+    // click drag
+    private boolean derDrag;
+    private float dragX;
+    private float dragY;
+    // kept track with timer
+    private boolean derMoni;
+    private float trackX;
+    private float trackY;
+    //click only
+    private boolean gotClick;
+    private long clickTime;
+    // current time
+    private long updater;
 }
