d361836176a2e31dfd7303b18d09d079693f3907
javier <javier@jnav.us>
2014-10-19 22:21:05 -0700

    finished on touch method

--- src/com/AnimationNonGradle/DucView.java
+++ src/com/AnimationNonGradle/DucView.java
@@ -64,12 +64,74 @@ public class DucView extends View {
      */
     @Override
     public boolean onTouchEvent(MotionEvent dahEvent)
-    {return true; // change this too
+    {
+        float onTEventX =dahEvent.getX();
+        float onTEventY =dahEvent.getY();
+        int action = dahEvent.getAction();
+
+
+        if(derMoto)
+        {
+            if(action == MotionEvent.ACTION_DOWN)
+            {
+                if(derX <= onTEventX && derX + derDuc.getWidth() > onTEventX && derY <= onTEventY && derY+ derDuc.getHeight() > onTEventY)
+                {
+                    clickTime = System.currentTimeMillis();
+                    gotClicked = true;
+                    //redraw
+                    invalidate();
+
+                }
+            }
+        }
+        else
+        {
+            if(action == MotionEvent.ACTION_DOWN)
+            {
+                if(derX <= onTEventX && derX + derDuc.getWidth() > onTEventX && derY <= onTEventY && derY+ derDuc.getHeight() > onTEventY)
+                {
+                    lastDragX= onTEventX;
+                    lastDragY= onTEventY;
+                    derDrag = true;
+
+                }
+                else // missing duck out of screen
+                {
+                    derX= onTEventX - derDuc.getWidth()/2;
+                    derY = onTEventY - derDuc.getHeight()/2;
+                   // redraw
+                    invalidate();
+
+                }
+
+            }
+            //finger drag
+            else if(action == MotionEvent.ACTION_MOVE && derDrag)
+            {
+                float deltX = onTEventX - lastDragX;
+                float deltY = onTEventY - lastDragY;
+
+                //apply delta
+                derX += deltX;
+                derY += deltY;
+                //redraw new location
+                invalidate();
+                //store current location
+                lastDragX = onTEventX;
+                lastDragY = onTEventY;
+            }
+        }
+
+
+
+
+        return true; // change this too
 
     }
     private Runnable updateDerState= new Runnable() {
-        @Override
-        public void run() {
+
+        public void run()
+        {
 
         }
     };
@@ -92,12 +154,12 @@ public class DucView extends View {
     private float derY;
     // click drag
     private boolean derDrag;
-    private float dragX;
-    private float dragY;
+    private float lastDragX;
+    private float lastDragY;
     // kept track with timer
-    private boolean derMoni;
-    private float trackX;
-    private float trackY;
+    private boolean derMoto;
+    private float trackPerSecX;
+    private float trackperSecY;
     //click only
     private boolean gotClicked;
     private long clickTime;
